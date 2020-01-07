3ed896326a431bbff0ab81339e3246720a9c917c
Michael Bragg <michael_bragg99@hotmail.com>
2020-01-06 17:36:06 -0700

    Test product imporation to products route in handlebars.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+const Sequelize = require('sequelize');
+const Product = require('../models/Product');
+
 var router = express.Router();
 
-const productSeeder = require('../seed/ProductSeeder');
+// const productSeeder = require('../seed/ProductSeeder');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Bork bork' });
+  Product.findAll()
+    .then(products => res.render('shop/index', { title: 'Bork bork', products: products }))
+    .catch(err => console.log(err));
 });
 
 router.get('/seed_products', (req, res) => {
