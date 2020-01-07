57e147510db17abfa3c613134951e70957ec4e93
Michael Bragg <michael_bragg99@hotmail.com>
2020-01-06 18:32:57 -0700

    Add user signup post route.

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/user/signup', (req, res, next) => {
   res.render('user/signup', { csrfToken: req.csrfToken() })
+});
+
+router.post('/user/signup', (req, res, next) => {
+  res.redirect('/');
 })
 
 module.exports = router;
